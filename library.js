5134bf0b440266fc6fd4736217d967b013a233e8
June <yoo.junehyuk@gmail.com>
2025-03-28 09:38:49 -0500

    Changed from Book objects to classes.
    Also encapsulated my library object (IIFE).

--- library.js
+++ library.js
@@ -1,93 +1,103 @@
-let myLibrary = [];
+//let myLibrary = [];
 
-function Book(title, author, pages, read = false) {
-    if (!new.target) {
-        throw Error("Must use the new keyword");
+class Book {
+    constructor(title, author, pages, read = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.uuid = crypto.randomUUID();
     }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.uuid = crypto.randomUUID();
-    this.info = function() {
+
+    info () {
         return this.read ? `${this.title} by ${this.author}, ${this.pages} pages, already read\n` : `${this.title} by ${this.author}, ${this.pages} pages, not yet read\n`;
     };
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
-}
+const library = (function() {
+    let myLibrary = [];
 
+    const getLibrary = () => myLibrary;
 
-function renderLibrary() {
-    const bookContainer = document.querySelector(".container");
-    bookContainer.innerHTML = "";
-    myLibrary.forEach((book) => {
-        const thisBook = document.createElement("div");
-        thisBook.classList.add("book");
+    const getBook = (uuid) => myLibrary.find((book) => book.uuid === uuid);
 
-        // Top Wrapper
-        const topWrapper = document.createElement("div");
-        topWrapper.setAttribute("style", "margin: 5px; padding: 0;");
+    const setLibrary = (newLibrary) => myLibrary = newLibrary;
 
-        const thisTitle = document.createElement("p");
-        thisTitle.setAttribute("style", "margin: 0; font-size: 20px; font-weight: bold;");
-        thisTitle.textContent = book.title;
+    const addBookToLibrary = function(title, author, pages, read) {
+        myLibrary.push(new Book(title, author, pages, read));
+    }
 
-        const thisAuthor = document.createElement("p");
-        thisAuthor.setAttribute("style", "margin: 0; font-size: 15px;");
-        thisAuthor.textContent = book.author;
+    const renderLibrary = function () {
+        const bookContainer = document.querySelector(".container");
+        bookContainer.innerHTML = "";
+        myLibrary.forEach((book) => {
+            const thisBook = document.createElement("div");
+            thisBook.classList.add("book");
+    
+            // Top Wrapper
+            const topWrapper = document.createElement("div");
+            topWrapper.setAttribute("style", "margin: 5px; padding: 0;");
+    
+            const thisTitle = document.createElement("p");
+            thisTitle.setAttribute("style", "margin: 0; font-size: 20px; font-weight: bold;");
+            thisTitle.textContent = book.title;
+    
+            const thisAuthor = document.createElement("p");
+            thisAuthor.setAttribute("style", "margin: 0; font-size: 15px;");
+            thisAuthor.textContent = book.author;
+    
+            const thisPages = document.createElement("p");
+            thisPages.setAttribute("style", "margin: 0; font-size: 15px; font-weight: 200;");
+            thisPages.textContent = `Page Count: ${book.pages}`;
+    
+            topWrapper.appendChild(thisTitle);
+            topWrapper.appendChild(thisAuthor);
+            topWrapper.appendChild(thisPages);
+    
+            // Bottom wrapper
+            const botWrapper = document.createElement("div");
+            botWrapper.setAttribute("style", "margin: 5px; padding: 0;");
+    
+            const thisStatus = document.createElement("p");
+            thisStatus.setAttribute("style", "margin: 10px 0 0; font-size: 20px; font-weight: 400;");
+            thisStatus.textContent = book.read ? "Status: Read" : "Status: Not Read";
+            thisStatus.style.color = book.read ? "blue" : "red";
+    
+            // Button wrapper in bottom wrapper
+            const buttonWrapper = document.createElement("div");
+            buttonWrapper.setAttribute("style", "margin: 0; display: flex;");
+    
+            const removeButton = document.createElement("button");
+            removeButton.classList.add("remove-button");
+            removeButton.setAttribute("data-uuid", book.uuid);
+            removeButton.textContent = "Remove";
+    
+            const changeStatusButton = document.createElement("button");
+            changeStatusButton.classList.add("change-status-button");
+            changeStatusButton.setAttribute("data-uuid", book.uuid);
+            changeStatusButton.textContent = "Change Status"
+    
+            buttonWrapper.appendChild(changeStatusButton);
+            buttonWrapper.appendChild(removeButton);
+    
+            botWrapper.appendChild(thisStatus);
+            botWrapper.appendChild(buttonWrapper);
+    
+            // Add wrappers to book div, then book div to container
+            thisBook.appendChild(topWrapper);
+            thisBook.appendChild(botWrapper);
+            bookContainer.appendChild(thisBook);
+        });
+    }
 
-        const thisPages = document.createElement("p");
-        thisPages.setAttribute("style", "margin: 0; font-size: 15px; font-weight: 200;");
-        thisPages.textContent = `Page Count: ${book.pages}`;
-
-        topWrapper.appendChild(thisTitle);
-        topWrapper.appendChild(thisAuthor);
-        topWrapper.appendChild(thisPages);
-
-        // Bottom wrapper
-        const botWrapper = document.createElement("div");
-        botWrapper.setAttribute("style", "margin: 5px; padding: 0;");
-
-        const thisStatus = document.createElement("p");
-        thisStatus.setAttribute("style", "margin: 10px 0 0; font-size: 20px; font-weight: 400;");
-        thisStatus.textContent = book.read ? "Status: Read" : "Status: Not Read";
-        thisStatus.style.color = book.read ? "blue" : "red";
-
-        // Button wrapper in bottom wrapper
-        const buttonWrapper = document.createElement("div");
-        buttonWrapper.setAttribute("style", "margin: 0; display: flex;");
-
-        const removeButton = document.createElement("button");
-        removeButton.classList.add("remove-button");
-        removeButton.setAttribute("data-uuid", book.uuid);
-        removeButton.textContent = "Remove";
-
-        const changeStatusButton = document.createElement("button");
-        changeStatusButton.classList.add("change-status-button");
-        changeStatusButton.setAttribute("data-uuid", book.uuid);
-        changeStatusButton.textContent = "Change Status"
-
-        buttonWrapper.appendChild(changeStatusButton);
-        buttonWrapper.appendChild(removeButton);
-
-        botWrapper.appendChild(thisStatus);
-        botWrapper.appendChild(buttonWrapper);
-
-        // Add wrappers to book div, then book div to container
-        thisBook.appendChild(topWrapper);
-        thisBook.appendChild(botWrapper);
-        bookContainer.appendChild(thisBook);
-    });
-}
+    return {getLibrary, setLibrary, getBook, renderLibrary, addBookToLibrary};
+})();
 
 // Example Books
-addBookToLibrary("Harry Potter", "J.K. Rowling", 129, false);
-addBookToLibrary("Red Rising", "Pierce Brown", 350, true);
-addBookToLibrary("The Way of Kings", "Brandon Sanderson", 550, true);
-console.table(myLibrary);
-renderLibrary();
+library.addBookToLibrary("Harry Potter", "J.K. Rowling", 129, false);
+library.addBookToLibrary("Red Rising", "Pierce Brown", 350, true);
+library.addBookToLibrary("The Way of Kings", "Brandon Sanderson", 550, true);
+library.renderLibrary();
 
 // Check's for form button click
 const bookForm = document.getElementById("book-form");
@@ -100,21 +110,21 @@ bookForm.addEventListener("submit", (e) => {
     const status = document.querySelector('select[name="status"]').value;
     const read = status === "has-read" ? true : false;
 
-    addBookToLibrary(title, author, pages, read);
-    renderLibrary();
+    library.addBookToLibrary(title, author, pages, read);
+    library.renderLibrary();
 });
 
 // Add remove button functionality
 document.querySelector(".container").addEventListener("click", (e) => {
+    const lib = library.getLibrary();
     if (e.target.matches(".remove-button")) {
       const toRemove = e.target.dataset.uuid;
-      myLibrary = myLibrary.filter((b) => b.uuid !== toRemove);
-      renderLibrary();
+      library.setLibrary(lib.filter((b) => b.uuid !== toRemove));
+      library.renderLibrary();
     } else if (e.target.matches(".change-status-button")) {
-        console.log("here");
         const toChange = e.target.dataset.uuid;
-        const bookToChange = myLibrary.find((book) => book.uuid === toChange);
+        const bookToChange = library.getBook(toChange);
         bookToChange.read = !bookToChange.read;
-        renderLibrary();
+        library.renderLibrary();
     }
   });
